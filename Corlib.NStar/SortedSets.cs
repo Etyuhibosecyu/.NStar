e08193e752662953d8bb72e5639921d7d5c8f2ec
Николай Этюгибосэкю <etyuhibosecyu@gmail.com>
2022-11-20 13:04:06 +0300

    Improve SortedSet<T>

--- Corlib.NStar/SortedSets.cs
+++ Corlib.NStar/SortedSets.cs
@@ -13,6 +13,8 @@ public abstract class SortedSetBase<T, TCertain> : SetBase<T, TCertain> where TC
 		set => throw new NotSupportedException();
 	}
 
+	public abstract IComparer<T> Comparer { get; }
+
 	public override TCertain Add(T item)
 	{
 		TryAdd(item);
@@ -29,9 +31,56 @@ public abstract class SortedSetBase<T, TCertain> : SetBase<T, TCertain> where TC
 		return ret >= index && ret < index + count ? ret : -1;
 	}
 
+	internal virtual int IndexOfNotLess(T item)
+	{
+		if (item == null)
+			throw new ArgumentNullException(nameof(item));
+		int ret = Search(item);
+		return ret >= 0 ? ret : ~ret;
+	}
+
 	private protected override TCertain InsertInternal(int index, IEnumerable<T> collection) => throw new NotSupportedException();
 
 	public override TCertain Insert(int index, T item) => throw new NotSupportedException();
+
+	private protected abstract void InsertInternal(int index, T item);
+
+	internal static int NthAbsent<TCertain2>(SortedSetBase<int, TCertain2> set, int n) where TCertain2 : SortedSetBase<int, TCertain2>, new()
+	{
+		if (set == null)
+			throw new ArgumentNullException(nameof(set));
+		if (set.Comparer != G.Comparer<int>.Default)
+			throw new ArgumentException("Множество должно иметь стандартный для int компаратор.", nameof(set));
+		if (set[0] < 0)
+			throw new ArgumentException("Не допускается множество, содержащее отрицательные значения.", nameof(set));
+		int lo = 0;
+		int hi = set.Length - 1;
+		var comparer = G.Comparer<int>.Default;
+		while (lo <= hi)
+		{
+			// i might overflow if lo and hi are both large positive numbers. 
+			int i = lo + ((hi - lo) >> 1);
+			int c;
+			try
+			{
+				c = comparer.Compare(set[i] - i, n);
+			}
+			catch (Exception ex)
+			{
+				throw new InvalidOperationException(null, ex);
+			}
+			if (c == 0) return n + i + 1;
+			if (c < 0)
+			{
+				lo = i + 1;
+			}
+			else
+			{
+				hi = i - 1;
+			}
+		}
+		return n + lo;
+	}
 
 	internal override TCertain ReplaceRangeInternal(int index, int count, IEnumerable<T> collection) => throw new NotSupportedException();
 
@@ -115,7 +164,7 @@ public abstract class SortedSet<T, TCertain> : SortedSetBase<T, TCertain> where 
 
 	public override int Capacity { get => items.Capacity; set => items.Capacity = value; }
 
-	public virtual IComparer<T> Comparer { get; }
+	public override IComparer<T> Comparer { get; }
 
 	public override int Length => items.Length;
 
@@ -147,7 +196,7 @@ public abstract class SortedSet<T, TCertain> : SortedSetBase<T, TCertain> where 
 		return item;
 	}
 
-	private protected virtual void InsertInternal(int index, T item) => items.Insert(index, item);
+	private protected override void InsertInternal(int index, T item) => items.Insert(index, item);
 
 	public override int Search(T item) => items.BinarySearch(item, Comparer);
 
